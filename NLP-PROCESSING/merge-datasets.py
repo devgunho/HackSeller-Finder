632c241e215763d687a28c27ae7afbb62f5c13c4
devgunho <devgun@kakao.com>
2021-05-28 22:01:17 +0900

    Modify merge text

--- NLP-PROCESSING/merge-datasets.py
+++ NLP-PROCESSING/merge-datasets.py
@@ -1,5 +1,9 @@
+# %%
 import pandas as pd
 import os
+
+from pandas.core.frame import DataFrame
+# %%
 
 
 def print_files_in_dir(root_dir, prefix):
@@ -14,13 +18,19 @@ def print_files_in_dir(root_dir, prefix):
     return file_list
 
 
+# %%
 root_dir = "../DUCKDUCKGO-CRAWLER/datasets/"
 final_file_list = print_files_in_dir(root_dir, "")
+print("target list:", len(final_file_list), final_file_list)
 
-f = open('merged_text.txt', 'w', encoding='utf-8', newline='')
+# %%
+result_df = DataFrame()
+
 for single_file in final_file_list:
     df = pd.read_csv(single_file)
-    for i in range(0, len(df)):
-        print(df['text'][i])
-        f.writelines(str(df['text'][i]))
-f.close
+    category_str = single_file.split('/')[-1].split('.')[0]
+    df.insert(0, 'category', category_str)
+    # print(df)
+    result_df = pd.concat([result_df, df])
+
+result_df.to_csv('merged_text.csv', index=False)
